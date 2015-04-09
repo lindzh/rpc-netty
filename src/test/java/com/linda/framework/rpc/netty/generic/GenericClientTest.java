e922a0b276d76ee37cc731fd5b04c1d75d218dbc
linda <linsony0@163.com>
2015-04-09 13:27:15 +0800

    misc:fix rpc test

--- src/test/java/com/linda/framework/rpc/netty/generic/GenericClientTest.java
+++ src/test/java/com/linda/framework/rpc/netty/generic/GenericClientTest.java
@@ -17,19 +17,19 @@ public class GenericClientTest {
 		client.startService();
 		GenericService service = client.register(GenericService.class);
 		
-		String[] getBeanTypes = new String[]{"com.linda.framework.rpc.netty.TestBean","int"};
+		String[] getBeanTypes = new String[]{"com.linda.framework.rpc.TestBean","int"};
 		HashMap<String,Object> map = new HashMap<String,Object>();
 		map.put("limit", 111);
 		map.put("offset", 322);
 		map.put("order", "trtr");
 		map.put("message", "this is a test");
 		Object[] getBeanArgs = new Object[]{map,543543};
-		Object hh = service.invoke("com.linda.framework.rpc.netty.HelloRpcService", RpcUtils.DEFAULT_VERSION, "getBean", getBeanTypes, getBeanArgs);
+		Object hh = service.invoke("com.linda.framework.rpc.HelloRpcService", RpcUtils.DEFAULT_VERSION, "getBean", getBeanTypes, getBeanArgs);
 		System.out.println(hh);
 		
 		String[] argTypes = new String[]{"java.lang.String","int"};
 		Object[] args = new Object[]{"hello,this is linda",543543};
-		service.invoke("com.linda.framework.rpc.netty.HelloRpcService", RpcUtils.DEFAULT_VERSION, "sayHello", argTypes, args);
+		service.invoke("com.linda.framework.rpc.HelloRpcService", RpcUtils.DEFAULT_VERSION, "sayHello", argTypes, args);
 
 	}
 
